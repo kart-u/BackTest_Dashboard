4a0b3ed9f3aa879764e512b49cfca58e525bb2ad
kart-u <pandeykartik715@gmail.com>
2025-07-30 21:06:05 +0000

    nginx setup to run locally just do [docker-compose up] then visit localhost

--- backend/src/main.py
+++ backend/src/main.py
@@ -3,24 +3,9 @@ from API.Home import main as home
 from API.Strategy import main as strategy
 from fastapi.middleware.cors import CORSMiddleware
 
-origins=[
-    "*"
-]
 
 app=FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-@app.get("/")
-def root():
-    return {'goofball'}
 
 app.include_router(home.router,prefix="/api",tags=['HOME'])
 app.include_router(strategy.router,prefix="/api",tags=['STRATEGY'])
